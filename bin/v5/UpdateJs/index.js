16df907b20b8dc2e0f14acbcf823ff66142a6fe9
keshavsoft <nknnkumar@yahoo.com>
2026-06-29 21:43:04 +0530

    5 to test

--- bin/v5/UpdateJs/index.js
+++ bin/v5/UpdateJs/index.js
@@ -1,36 +1,10 @@
-// v2/AppJs/index.js
-
-import checkLines from "./checkLines.json" with {type: "json"};
 import alterFile from "./common/AlterFile/index.js";
-
-const validateCheckLines = (obj) => {
-    if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
-        throw new TypeError("inCheckLines must be a valid object.");
-    }
-    for (const key of ["importLines", "useLines"]) {
-        if (!(key in obj)) {
-            throw new Error(`inCheckLines must contain "${key}".`);
-        }
-        const section = obj[key];
-        if (typeof section !== "object" || section === null || Array.isArray(section)) {
-            throw new TypeError(`inCheckLines.${key} must be an object.`);
-        }
-        if (typeof section.toInsertLine !== "string") {
-            throw new TypeError(`inCheckLines.${key}.toInsertLine must be a string.`);
-        }
-        if (typeof section.duplicationCheck !== "string") {
-            throw new TypeError(`inCheckLines.${key}.duplicationCheck must be a string.`);
-        }
-        if (!Array.isArray(section.insertAfter)) {
-            throw new TypeError(`inCheckLines.${key}.insertAfter must be an array.`);
-        }
-    }
-};
+import validateCheckLines from "./validateCheckLines.js";
 
 const updateAppJs = ({ inJsFilePath, inCheckLines,
     showLog = false }) => {
 
-    const localCheckLines = (inCheckLines && Object.keys(inCheckLines).length > 0) ? inCheckLines : checkLines;
+    const localCheckLines = inCheckLines;
     
     validateCheckLines(localCheckLines);
 
