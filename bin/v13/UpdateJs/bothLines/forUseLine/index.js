0bc1a52d9e0f18d9332b12718f4e0f0f5d9695c8
keshavsoft <nknnkumar@yahoo.com>
2026-08-02 14:56:46 +0530

    to test

---
+++ bin/v13/UpdateJs/bothLines/forUseLine/index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+import defaultFunc from 'pattern-collector-anyjs-story';
+import insertUseLine from "./insertUseLine.js";
+
+const startFunc = ({ inFolderNameToInsert, inFileType, inJsPath }) => {
+    const folderNameToInsert = inFolderNameToInsert;
+    const localJsPath = inJsPath;
+
+    const fileContent = fs.readFileSync(localJsPath, 'utf8');
+
+    const story = defaultFunc({
+        fileContent,
+        fileType: inFileType
+    });
+
+    insertUseLine({
+        inStory: story,
+        fileContent,
+        filePath: localJsPath, inFolderNameToInsert: folderNameToInsert,
+        inTemplate1: story.regexForPullLinesStory.consumptionRegex.reverseTemplate,
+        inTemplate: story.reverseTemplates.consumptionRegex,
+        inParts: [folderNameToInsert, `${story.variablesConnection}${folderNameToInsert}`]
+    });
+};
+
+export default startFunc;
+
+// startFunc({ folderNameToInsert, fileType });
