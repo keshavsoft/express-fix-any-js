b32d3c08b7acf6c4d3c6c953babbdb0e4ff9fb19
keshavsoft <nknnkumar@yahoo.com>
2026-08-04 11:48:55 +0530

    25 all checked

---
+++ bin/v25/alterFile/start.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+
+import fileNamesJson from '../fileNames.json' with {type: 'json'};
+
+const startFunc = ({ alterArray, inFileType, inTargetPath, inValue }) => {
+
+    try {
+        if (!inFileType in fileNamesJson) {
+            return false;
+        };
+
+        if (!"hookTo" in fileNamesJson[inFileType]) {
+            return false;
+        };
+
+        if (!"fileName" in fileNamesJson[fileNamesJson[inFileType]?.hookTo]) {
+            return false;
+        };
+
+        const jsFileName = fileNamesJson[fileNamesJson[inFileType]?.hookTo]?.fileName;
+
+        const localJsPath = path.join(inTargetPath, inValue, jsFileName);
+
+        let fileContent = fs.readFileSync(localJsPath, 'utf8');
+
+        alterArray.forEach(element => {
+            fileContent = fileContent.replaceAll(element.key, element.value);
+        });
+
+        fs.writeFileSync(localJsPath, fileContent);
+
+        return true;
+
+    } catch (error) {
+        console.log("error : ", error);
+
+    };
+};
+
+export default startFunc;
---
+++ bin/v25/hookFolder/start.js
@@ -0,0 +1,25 @@
+import forImportLine from "./forImportLine/index.js";
+
+import fileNamesJson from '../fileNames.json' with {type: 'json'};
+
+const startFunc = ({ inValue, OutValue,
+    inFileType, inTargetPath }) => {
+
+    const fromImportLine = forImportLine({
+        inFileType, regexKey: "import",
+        templateKey: "importRegex",
+        inTargetPath, presentKey: "importLines",
+        inParts: [`${fileNamesJson[inFileType]?.temporaryValue}${inValue}`, inValue]
+    });
+
+    const fromUseLine = forImportLine({
+        inParts: [`${fileNamesJson[inFileType]?.temporaryValue}${inValue}`, OutValue],
+        inFileType, regexKey: "consumption",
+        templateKey: "consumptionRegex",
+        inTargetPath, presentKey: "useLines"
+    });
+
+    return { fromImportLine, fromUseLine }
+};
+
+export default startFunc;
