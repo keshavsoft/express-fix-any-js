92a536f9fb991d1122ef2f135054c9a0cfd24af2
keshavsoft <nknnkumar@yahoo.com>
2026-08-05 13:03:36 +0530

    to check for duplicate

--- test/v25/forRoutesJsEnd/test.js
+++ test/v25/forRoutesJsEnd/test.js
@@ -4,11 +4,12 @@ import { fileURLToPath } from "url";
 import index from "../../../index.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const tableName = "table1";
 
 const output = index({
     inFileType: "fromRoutesJsEnd",
     inTargetPath: __dirname,
-    inValue: "doc1", OutValue: "doc2"
+    inValue: tableName, OutValue: tableName
 });
 
 console.log("aaaaaaa : ", output);
